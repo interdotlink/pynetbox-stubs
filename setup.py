7e6badeaabf550b96bc31534af9ac4eb3f3075a6
Christian Harendt <22472485+christianharendt@users.noreply.github.com>
2023-09-20 10:23:21 +0200

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description="PEP 561 type stubs for pynetbox",
     url="",
     license="Apache License 2.0",
-    version="0.0.2",
+    version="0.0.3",
     packages=["pynetbox-stubs"],
     # PEP 561 requires these
     install_requires=[
